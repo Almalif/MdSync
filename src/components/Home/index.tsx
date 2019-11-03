582afd70912ff9599a97d47cae68645ca33da3bf
Timour Almazov <talmazov@greenflex.com>
2019-11-03 23:18:58 +0100

    add file name

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Button } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Input } from 'semantic-ui-react';
 import Router from 'next/router';
 import { SemanticToastContainer, toast } from 'react-semantic-toasts';
 import * as Network from '../../utils/Network';
 
 const Home = () => {
+  const [filename, setFilename] = useState<string>('')
   const createFile = async () => {
     try {
       const response = await Network.post({
         endpoint: '/files',
         params: {
-          name: 'putos3',
+          name: filename,
         },
       });
       if (response && response.data && response.data.id) {
@@ -37,6 +38,7 @@ const Home = () => {
   };
   return (
     <div>
+      <Input placeholder="File name.." onChange={(_, { value })  => {setFilename(value)}} />
       <Button size="massive" onClick={createFile}>
         Create file
       </Button>
