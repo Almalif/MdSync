211bc607aa8442c1ea9e27c479024aa595e78f35
Almazov <21504153+tim92i@users.noreply.github.com>
2019-11-09 08:05:31 +0100

    add i18n part2 (#12)
    add i18n translation files

---
+++ src/pages/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import nextI18NextMiddleware from 'next-i18next/middleware';
+import next from 'next';
+import nextI18next from '../utils/i18n';
+
+const port = process.env.PORT || 3000;
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handle = app.getRequestHandler();
+
+(async () => {
+  await app.prepare();
+  const server = express();
+
+  server.use(nextI18NextMiddleware(nextI18next));
+
+  server.get('*', (req, res) => handle(req, res));
+
+  await server.listen(port);
+  console.log(`> Ready on http://localhost:${port}`); // eslint-disable-line no-console
+})();
